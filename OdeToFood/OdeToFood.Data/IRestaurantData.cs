e9510b71701f89c348b7ec3d74c82f9de8710fa5
jakubgerlee <jakubgerlee@gmail.com>
2020-02-20 15:28:56 +0100

    Initial project with list all restaurants and details of them.

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+	public interface IRestaurantData
+	{
+		IEnumerable<Restaurant> GetRestaurantByName(string name);
+		Restaurant GetRestaurantById(int id);
+	}
+
+	public class InMemoryRestaurantData: IRestaurantData
+	{
+		private readonly List<Restaurant> _restaurants;
+		public InMemoryRestaurantData()
+		{
+			_restaurants = new List<Restaurant>
+			{
+				 new Restaurant
+				 {
+					 Id = 1,
+					 Name = "Scott's Pizza",
+					 Cuisine = CuisineType.Italian,
+					 Location = "MaryLand"
+				 },
+				 new Restaurant
+				 {
+					 Id = 2,
+					 Name = "Mehikano",
+					 Cuisine = CuisineType.Mexican,
+					 Location = "Warsaw"
+				 },
+				 new Restaurant
+				 {
+					 Id = 3,
+					 Name = "Mariano Italiano",
+					 Cuisine = CuisineType.Italian,
+					 Location = "MaryLand"
+				 },
+				 new Restaurant
+				 {
+					 Id = 4,
+					 Name = "Hindian",
+					 Cuisine = CuisineType.Indian,
+					 Location = "MaryLand"
+				 }
+			};
+		}
+
+		public IEnumerable<Restaurant> GetRestaurantByName(string name)
+		{
+			return from restaurant in _restaurants
+				where string.IsNullOrEmpty(name) ||
+				      restaurant.Name.StartsWith(name, StringComparison.CurrentCultureIgnoreCase)
+				orderby restaurant.Name
+				select restaurant;
+		}
+
+		public Restaurant GetRestaurantById(int id)
+		{
+			return _restaurants.SingleOrDefault(x => x.Id == id);
+		}
+	}
+}
---
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood
+{
+    public class ListModel : PageModel
+    {
+	    private readonly IConfiguration _configuration;
+	    private readonly IRestaurantData _restaurantData;
+
+	    public string Message { get; set; }
+
+		[BindProperty(SupportsGet = true)]
+	    public string SearchTerm { get; set; }
+	    
+	    public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration configuration, IRestaurantData restaurantData)
+        {
+	        _configuration = configuration;
+	        _restaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+	        Message = _configuration["Message"];
+	        Restaurants = _restaurantData.GetRestaurantByName(SearchTerm);
+        }
+    }
+}
